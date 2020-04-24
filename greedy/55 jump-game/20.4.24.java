ffdedc2375b20d3306ab25d1b777ecf44210e9d8
Li JinFeng <18936035049@163.com>
2020-04-25 00:50:47 +0800

    Update 20.4.24.java

--- greedy/55 jump-game/20.4.24.java
+++ greedy/55 jump-game/20.4.24.java
@@ -2,13 +2,13 @@
     public boolean canJump(int[] nums) {
         if(nums.length < 2 ){
             return true;
-        }
-        int max_length = nums[0];
+        }                                       //判断边界
+        int max_length = nums[0];               //初始化
         for(int i = 0;i<nums.length;i++){
             if(i>max_length){
-                return false;
-            }else{ max_length = Math.max(max_length,nums[i]+i);}
-            if(max_length >= nums.length-1) {return true;}
+                return false;                   //如果当前下标 之前的都到不了 就返回false
+            }else{ max_length = Math.max(max_length,nums[i]+i);} //如果当前下标可以到达 更新最大可达距离
+            if(max_length >= nums.length-1) {return true;}      //如果可以到达最远点 返回true
         }
         return false;
  }
