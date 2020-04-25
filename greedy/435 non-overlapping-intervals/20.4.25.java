28885ff563078df45ea8b53dffee04910b22f998
Li JinFeng <18936035049@163.com>
2020-04-25 19:44:17 +0800

    Update 20.4.25.java

--- greedy/435 non-overlapping-intervals/20.4.25.java
+++ greedy/435 non-overlapping-intervals/20.4.25.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i = 1;i<intervals.length;i++){
             if(intervals[i][0]<intervals[i-1][1]){          //如果下一个区间的左端点小于上一个区间的右端点 有交集 那么得移除一个区间 
                 intervals[i][1] = intervals[i][1] > intervals[i-1][1]?intervals[i-1][1]:intervals[i][1];  
-                ans++;                                      //将右端点更新为两者较小值 减少对之后区间的影响 是贪心算法的本质 当前解为最终解之一
+                ans++;                                      //将右端点更新为两者较小值 减少对之后区间的影响 是贪心算法的本质 当前解为最终解部分
             }
         }
         return ans;
