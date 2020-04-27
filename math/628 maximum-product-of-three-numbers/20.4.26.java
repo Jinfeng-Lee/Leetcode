48aacb64bbb93c1fb300cecc6b31d3c06d982f2d
Li JinFeng <18936035049@163.com>
2020-04-27 16:04:06 +0800

    Update 20.4.26.java

--- math/628 maximum-product-of-three-numbers/20.4.26.java
+++ math/628 maximum-product-of-three-numbers/20.4.26.java
@@ -4,3 +4,4 @@ class Solution {
         return Math.max(nums[0]*nums[1]*nums[nums.length-1],nums[nums.length-3]*nums[nums.length-2]*nums[nums.length-1]);
     }
 }
+//这题根据数学归纳法 排序后要么在前三个数 要么是后两个数乘最大的数
