0e5b8d834cc3f83f204bcf1e3cef34f9cde5b5fc
Li JinFeng <18936035049@163.com>
2020-06-03 17:20:05 +0800

    Update 20.6.2.java

--- dynamic programming/309 best-time-to-buy-and-sell-stock-with-cooldown/20.6.2.java
+++ dynamic programming/309 best-time-to-buy-and-sell-stock-with-cooldown/20.6.2.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if(len < 2){
+            return 0;
+        }
+        int[][] dp = new int[2][3];//滚动数组
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;         //初始化
+        
+        for(int i = 1;i<prices.length;i++){
+            dp[i & 1][0] = Math.max(dp[(i-1) & 1][0],dp[(i-1) & 1][1] + prices[i]); //0 只能由0 1 转换过来
+            dp[i & 1][1] = Math.max(dp[(i-1) & 1][1],dp[(i-1) & 1][2] - prices[i]); //1 只能由1 2 转换过来
+            dp[i & 1][2] = dp[(i-1) & 1][0];                                        //2 只能由 0 转换过来    
+        }
+        return Math.max(dp[(len-1) & 1][0],dp[(len-1) & 1][2]);
+    }
+}
