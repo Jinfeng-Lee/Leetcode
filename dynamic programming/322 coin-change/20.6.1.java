b29066362c11523428b945968c1e99e4e43ea070
Li JinFeng <18936035049@163.com>
2020-07-01 20:35:35 +0800

    Update 20.6.1.java

--- dynamic programming/322 coin-change/20.6.1.java
+++ dynamic programming/322 coin-change/20.6.1.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount+1;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,max);
+        dp[0] = 0;
+        for(int i =0;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                if(i>=coins[j]){
+                    dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[amount] == max?-1:dp[amount];
+    }
+}
