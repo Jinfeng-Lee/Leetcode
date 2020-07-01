6079cc73d20627ebd889bbd105d79219708adcec
Li JinFeng <18936035049@163.com>
2020-07-01 21:23:47 +0800

    Update 20.5.27.java

--- dynamic programming/279 perfect-squares/20.5.27.java
+++ dynamic programming/279 perfect-squares/20.5.27.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int numSquares(int n) {
+        int count = (int)Math.pow(n,0.5);       //注意两个参数都是double
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,n+1);
+        dp[0] = 0;
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=count;j++){
+                if(i>=(int)Math.pow(j,2)){
+                    dp[i] = Math.min(dp[i],dp[i-(int)Math.pow(j,2)]+1);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
