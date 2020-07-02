8d996b649fa9bab0b5bb41cf168e18c4d37a5a68
Li JinFeng <18936035049@163.com>
2020-07-02 21:07:09 +0800

    Update 20.6.27.java

--- dynamic programming/338 counting-bits/20.6.27.java
+++ dynamic programming/338 counting-bits/20.6.27.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int[] countBits(int num) {
+        if(num == 0){ return new int[1];}
+        int count = 0;
+        int[] ans = new int[num+1];
+        ans[0] = 0;
+        ans[1] = 1;
+        for(int i =2;i<=num;i++){
+            if(i==Math.pow(2,count+1)){
+                count++;
+                ans[i] = 1 + ans[i-(int)Math.pow(2,count)];
+            }else{
+                ans[i] = 1 + ans[i-(int)Math.pow(2,count)];
+            }
+            
+        }
+        return ans;
+    }
+}   
