4c343802a5e37f5a81778fa8ccad945d9f0b89ae
Li JinFeng <18936035049@163.com>
2020-07-05 21:18:31 +0800

    Create 20.7.5.java

---
+++ dynamic programming/416 partition-equal-subset-sum/20.7.5.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int len = nums.length;
+        int sum = 0;
+        for(int num:nums){
+            sum+= num;
+        }
+        if((sum & 1) == 1){
+            return false;
+        }
+        int target = sum/2;
+        boolean[][] dp = new boolean[len][target+1];
+        if(nums[0] <= target){
+            dp[0][nums[0]] = true;
+        }
+        for(int i = 1;i<len;i++){
+            for(int j = 0;j<=target;j++){
+                dp[i][j] = dp[i-1][j];
+                if(nums[i] == j){
+                    dp[i][j] = true;
+                }
+                if(nums[i]<=j){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]];
+                }
+            }
+        }
+        return dp[len-1][target];
+    }
+}
+//典型的0 1 背包问题
