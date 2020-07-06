554bcf9e98958e8fe5356c97f660b47f275c3a69
Li JinFeng <18936035049@163.com>
2020-07-06 23:14:33 +0800

    Update 20.5.31.java

--- stack/739 daily-temperatures/20.5.31.java
+++ stack/739 daily-temperatures/20.5.31.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int len = T.length;
+        int[] ans = new int[len];
+        Deque<Integer> deque = new LinkedList<Integer>();
+        for(int i = 0;i<len;i++){
+            while(!deque.isEmpty() && T[i]>T[deque.peek()]){
+                int temp = deque.pop();
+                ans[temp] = i-temp;
+            } 
+            deque.push(i);
+        }
+        return ans;
+    }
+}
+//单调栈
