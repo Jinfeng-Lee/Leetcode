616beb272cf636565d1df237f6f046c0e02f1c4f
Li JinFeng <18936035049@163.com>
2020-07-12 00:08:36 +0800

    Update 20.7.11.java

--- 剑指offer/21 diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/20.7.11.java
+++ 剑指offer/21 diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/20.7.11.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int[] exchange(int[] nums) {
+        int right  = nums.length-1;
+        int i = 0;
+        while(i<right){
+            if(nums[i] % 2 == 1){
+                i++;
+            }else{
+                int temp = nums[right];
+                nums[right] = nums[i];
+                nums[i] = temp;
+                right--;
+            }
+        }
+        return nums;
+    }
+}
