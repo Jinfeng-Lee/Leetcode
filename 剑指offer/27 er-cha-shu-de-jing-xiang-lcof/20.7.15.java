eda10f75a1086ce92f120b30d8c3338933263933
Li JinFeng <18936035049@163.com>
2020-07-16 09:48:44 +0800

    Update 20.7.15.java

--- 剑指offer/27 er-cha-shu-de-jing-xiang-lcof/20.7.15.java
+++ 剑指offer/27 er-cha-shu-de-jing-xiang-lcof/20.7.15.java
@@ -1,1 +1,24 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null){ return null;}
+        return recur(root);
+    }
+    public TreeNode recur(TreeNode root){
+        if(root == null){ return null;}
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        recur(root.left);
+        recur(root.right);
+        return root;
+    }
+}
