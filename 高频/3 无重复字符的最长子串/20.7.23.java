83b967b9656f9a1a1cce83bdbc59a48d60115184
Li JinFeng <18936035049@163.com>
2020-07-24 00:27:41 +0800

    Update 20.7.23.java

--- 高频/3 无重复字符的最长子串/20.7.23.java
+++ 高频/3 无重复字符的最长子串/20.7.23.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0){return 0;}
+        HashMap<Character,Integer> cur = new HashMap();
+        int ans = 0;
+        int left = 0;
+        for(int i = 0;i<s.length();i++){
+            char temp = s.charAt(i);
+            if(cur.containsKey(temp)){
+                left = Math.max(left,cur.get(temp)+1);
+            }
+            cur.put(temp,i);
+            ans = Math.max(ans,i-left+1);
+        }
+        return ans;
+    }
+}
