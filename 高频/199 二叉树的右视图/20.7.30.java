775002e314a078c42db1ef45ada6bb7619252807
Li JinFeng <18936035049@163.com>
2020-07-31 00:17:38 +0800

    Update 20.7.30.java

--- 高频/199 二叉树的右视图/20.7.30.java
+++ 高频/199 二叉树的右视图/20.7.30.java
@@ -1,1 +1,27 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList();
+        if(root == null){ return ans;}
+        Queue<TreeNode> cur = new LinkedList();
+        cur.offer(root);
+        while(!cur.isEmpty()){
+            int size = cur.size();
+            for(int i =0;i<size;i++){
+                TreeNode temp = cur.poll();
+                if(temp.left!=null){ cur.offer(temp.left);}
+                if(temp.right!=null){ cur.offer(temp.right);}
+                if(i == size-1){ ans.add(temp.val);}
+            }
+        }
+        return ans;
+    }
+}
