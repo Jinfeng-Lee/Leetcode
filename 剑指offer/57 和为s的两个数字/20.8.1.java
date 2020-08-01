fab59c4270b90e1aa94f91dcc7b06b247a22e733
Li JinFeng <18936035049@163.com>
2020-08-01 11:29:03 +0800

    Create 20.8.1.java

---
+++ 剑指offer/57 和为s的两个数字/20.8.1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums.length == 1){ return new int[0];}
+        int left = 0;
+        int right = nums.length-1;
+        while(left<right){
+            if(nums[left] + nums[right] == target){
+                return new int[]{nums[left],nums[right]};
+            }else if(nums[left] + nums[right] < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return new int[0];
+    }
+}
