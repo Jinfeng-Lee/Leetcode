9ba259920299b3d6b669b68279ea9d15c68d830e
Li JinFeng <18936035049@163.com>
2020-08-15 20:51:37 +0800

    Create 20.8.15.java

---
+++ 剑指offer/26 树的子结构/20.8.15.java
@@ -0,0 +1,24 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if(root2 == null || root1 == null){ return false;}
+        return recur(root1,root2) || HasSubtree(root1.left,root2) || HasSubtree(root1.right,root2);
+    }
+    public boolean recur(TreeNode root1,TreeNode root2){
+        if(root2 == null){ return true;}
+        if(root1 == null || root1.val!=root2.val){ return false;}
+        return recur(root1.left,root2.left) && recur(root1.right,root2.right);
+    }
+}
