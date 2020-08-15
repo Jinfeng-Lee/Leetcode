2fc89985dc63ec8699619c40dd0f5cbb42725c79
Li JinFeng <18936035049@163.com>
2020-08-15 20:13:38 +0800

    Create 20.8.15.java

---
+++ 剑指offer/25 合并两个排序的链表/20.8.15.java
@@ -0,0 +1,30 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        if(list1 == null){ return list2;}
+        if(list2 == null){ return list1;}
+        ListNode header = new ListNode(0);
+        ListNode cur = header;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                cur.next = list1;
+                cur = cur.next;
+                list1 = list1.next;
+            }else{
+                cur.next = list2;
+                cur = cur.next;
+                list2 = list2.next;
+            }
+        }
+        cur.next = list1 == null?list2:list1;
+        return header.next;
+    }
+}
