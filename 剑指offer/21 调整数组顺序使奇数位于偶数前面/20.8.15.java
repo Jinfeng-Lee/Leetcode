6ef7da77cabe8b6193545a7c55dafa259c07b751
Li JinFeng <18936035049@163.com>
2020-08-15 19:03:53 +0800

    Update 20.8.15.java

--- 剑指offer/21 调整数组顺序使奇数位于偶数前面/20.8.15.java
+++ 剑指offer/21 调整数组顺序使奇数位于偶数前面/20.8.15.java
@@ -21,3 +21,26 @@ public class Solution {
     }
 }
 保持奇数偶数相对位置不变
+
+
+
+public class Solution {
+    public void reOrderArray(int [] array) {
+        if(array == null){ return;}
+        int count = 0;
+        for(int i = 0;i<array.length;i++){
+            if(array[i] %2 == 1){
+                int j = i;
+                while(j>count){
+                    int temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                    j--;
+                }
+                count++;
+            }
+        }
+        
+    }
+}
+//原地 基于插入排序
